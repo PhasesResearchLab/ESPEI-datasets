280550555b2d6c3fcccc529204d881ae8655c5a9
bocklund <brandonbocklund@gmail.com>
2018-03-23 20:28:57 -0400

    WIP: Try enabling CI completely

--- ci/git-commit-changes.sh
+++ ci/git-commit-changes.sh
@@ -3,9 +3,6 @@
 # We expect that this can "fail". If there are no changes, there should be nothing to commit.
 # Modified from https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
-echo "Travis branch"
-echo $TRAVIS_BRANCH
-
 # Set up git
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
@@ -15,8 +12,8 @@ git branch -v
 
 # add all the files and commit
 git add -A
-git commit -m "Travis CI ESPEI Dataset Linting"
+git commit -m "LINT: Travis CI automated linting [ci skip]"
 
 # upload
 git remote add origin https://${GH_TOKEN}@github.com/phasesresearchlab/espei-datasets.git > /dev/null 2>&1
-#git push --quiet --set-upstream origin $TRAVIS_BRANCH > /dev/null 2>&1
+git push --quiet --set-upstream origin $TRAVIS_BRANCH > /dev/null 2>&1
